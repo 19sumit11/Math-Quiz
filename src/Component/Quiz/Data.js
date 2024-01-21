112f6876f211a2afcb20e9d175a9d0346185625c
Sumit shriwastava <sumit87478@gmail.com>
2024-01-21 23:42:36 +0530

    question add on

--- src/Component/Quiz/Data.js
+++ src/Component/Quiz/Data.js
@@ -9,13 +9,107 @@ export const data = [
 
     },
     {
-        question:"Why is perimeter of circle ?",
-        option1: "π",
-        option2:"2r",
-        option3:"2πr",
-        option4:"2πr^2",
+        question:"What is area of circle ?",
+        option1: "πr*r",
+        option2:"2πr",
+        option3:"2π+r",
+        option4:"2πr*r",
         ans:1,
 
-    }
+    },
+    {
+        question:"What is circumference of circle ?",
+        option1: "πr",
+        option2:"2πr",
+        option3:"πr*r",
+        option4:"2πr*r",
+        ans:2,
+
+    },
+    {
+        question:" The area of a rhombus whose diagonals are of lengths 5 cm and 4.2 cm is:",
+        option1: " 41 cm2",
+        option2:" 14.5cm2",
+        option3:"10.5cm2",
+        option4:"20.5cm2",
+        ans:3,
+
+    },
+   
+    {
+        question:"The perimeter of a rectangle with length (l) and width (w) is:",
+        option1: "l+w",
+        option2:"2(l+w)",
+        option3:"(l+w)2",
+        option4:" lw",
+        ans:2,
+
+    },
+    {
+        question:"What is area of Rectangle ?",
+        option1: "ab",
+        option2:"2ab",
+        option3:"2(a+b)",
+        option4:"2a+2",
+        ans:1,
+
+    },
+    {
+        question:"Area of the square with side-length ‘a’ is:",
+        option1: " a2",
+        option2:" 2a",
+        option3:" 4a",
+        option4:" a/2",
+        ans:1,
+
+    },
+    {
+        question:"The area of a parallelogram with length (l) and breadth (b) is:",
+        option1: " ½ lb",
+        option2:" 2lb",
+        option3:" lb",
+        option4:" (lb)2",
+        ans:3,
+
+    },
+    {
+        question:"Volume of a cylinder with base radius = r and height = h, is:",
+        option1: " 2πrh",
+        option2:"  πr²h",
+        option3:"  2πr (r + h)",
+        option4:"  1/3 πr²h",
+        ans:2,
+
+    },
+    {
+        question:" Volume of a cuboid of length (l), width (w) and height (h) is:",
+        option1: " lbh",
+        option2:" lb + bh + hl",
+        option3:"2 (lb + bh + hl)",
+        option4:"2 (l + b) h",
+        ans:1,
+
+    },
+    {
+        question:" Surface area of cube of edge ‘a’ is:",
+        option1: "4a²",
+        option2:" 6a²",
+        option3:" 3a²",
+        option4:" a²",
+        ans:2,
+
+    },
+    {
+        question:" Area of a triangle with base (b) and height (h) is:",
+        option1: "b.h",
+        option2:" ½ b.h",
+        option3:"2b.h",
+        option4:"None of these",
+        ans:2,
+
+    },
+    
+    
+   
 ];
 
